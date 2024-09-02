32d2d1b9d4070a6a8d6c3a225a5e20e2e51498ab
Leonardo Fragoso <Leonardorfragoso@gmail.com>
2024-09-02 10:32:01 -0300

    Criando views de logout e cadastro de usuário

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-
-login_manager = LoginManager()
-db.init_app(app)
-login_manager.init_app(app)
-#view login
-
-#Session <- conexão ativa
-
-@app.route('/login', methods=["POST"])
 def login():
+
+  return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({"message": "Logout realizado com sucesso!"})
+
+@app.route('/user', methods=["POST"])
+def create_user():
   data = request.json
   username = data.get("username")
   password = data.get("password")
 
   if username and password:
-   # Login
-    user = User.query.filter_by(username=username).firts()
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "Usuario cadastrado com sucesso"})
 
-    if user and user.password == password:
-      return jsonify({"message": "Autenticação realizada com sucesso!"})
-
-  return jsonify({"message": "Credenciais inválidas"}), 400
+  return jsonify({"message": "Dados invalidos"}), 400
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
