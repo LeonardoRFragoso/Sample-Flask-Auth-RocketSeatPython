362fb9a01415dfc0376fde7da3d1a35989421558
Leonardo Fragoso <Leonardorfragoso@gmail.com>
2024-09-02 12:36:46 -0300

    criptografando senhas no BD

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql:///root:admin123@127.0.0.1:3306/flask-crud'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin123@127.0.0.1:3306/flask-crud'
 
 login_manager = LoginManager()
 db.init_app(app)
@@ -13,8 +14,6 @@ login_manager.init_app(app)
 #view login
 login_manager.login_view = 'login'
 #Session <- conexão ativa
-
-@login_manager.user_loader
 def load_user(user_id):
   return User.query.get(user_id)
 
@@ -26,7 +25,7 @@ def login():
   if username and password:
    # Login
     user = User.query.filter_by(username=username).first()
-    if user and user.password == password:
+    if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
       login_user(user)
       print(current_user.is_authenticated)
       return jsonify({"message": "Autenticação realizada com sucesso!"})
@@ -44,7 +43,8 @@ def create_user():
   username = data.get("username")
   password = data.get("password")
   if username and password:
-    user = User(username=username, password=password)
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    user = User(username=username, password=hashed_password, role='user')    
     db.session.add(user)
     db.session.commit()
     return jsonify({"message": "Usuario cadastrado com sucesso"})
